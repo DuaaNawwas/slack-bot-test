66aa4900f400bca26cb282eb72a4bb7475f09d29
AZZIE2000 <azzam.faraj0@gmail.com>
2023-09-10 15:44:30 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import fastify from "fastify";
-
+import cors from "@fastify/cors";
 const server = fastify();
 
 server.get("/", async (request, reply) => {
   return reply.code(200).send("<h1>hi</h1>");
 });
-
+server.post("/a", async (request, reply) => {
+  return reply.code(200).send({ message: "<h1>hi</h1>" });
+});
+server.register(cors, {
+  origin: "*",
+});
 server.listen({ port: 8080 }, (err, address) => {
   if (err) {
     console.error(err);
