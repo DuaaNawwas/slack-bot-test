145c535158982570cf4845a83c61f439dfac30a5
AZZIE2000 <azzam.faraj0@gmail.com>
2023-09-10 15:52:44 +0300

    update plzzz

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,17 @@ const app = Fastify();
 app.register(cors, {
   origin: "*",
 });
-
+const PORTNUM = process.env.PORT || 4000;
 app.get("/", async (request, reply) => {
   return reply.code(200).send("<h1>hi</h1>");
 });
 app.post("/a", async (request, reply) => {
   return reply.code(200).send({ message: "<h1>hi</h1>" });
 });
-app.listen({ port: 8080 }, (err, address) => {
+app.listen({ port: +PORTNUM }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
-  console.log(`Server listening at please http:localhost:${8080}`);
+  console.log(`Server listening at please http:localhost:${+PORTNUM}`);
 });
