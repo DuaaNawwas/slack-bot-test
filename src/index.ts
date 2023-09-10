d20b7a5595f74e3f37a2ffaf4bf31db03d01d381
AZZIE2000 <azzam.faraj0@gmail.com>
2023-09-10 15:30:53 +0300

    aaa

--- src/index.ts
+++ src/index.ts
@@ -11,5 +11,5 @@ server.listen({ port: 8080 }, (err, address) => {
     console.error(err);
     process.exit(1);
   }
-  console.log(`Server listening at ${address}`);
+  console.log(`Server listening at http:localhost:${8080}`);
 });
